f6bc0d6a781af1d4ac8dad9750f97a254509ddbc
jaxongir <j.niyazmetov@newuu.uz>
2025-02-27 22:24:45 +0500

    lesson 10 is done

---
+++ lesson-10/homework/weather.py
@@ -0,0 +1,34 @@
+import requests
+import json
+
+#My data
+API_KEY = "cb77f2e7599c26ba5a7057dbf4009af0"
+CITY = input("Enter City: ")
+BASE_URL = "http://api.openweathermap.org/data/2.5/weather"
+
+#Fixed
+params = {
+    "q": CITY,
+    "appid": API_KEY,
+    "units": "metric"
+}
+
+#Request
+response = requests.get(BASE_URL, params=params)
+data = response.json()
+# print(json.dumps(data, indent=4))
+
+#Output
+if response.status_code == 200:
+    temp = data["main"]["temp"]
+    humidity = data["main"]["humidity"]
+    weather_desc = data["weather"][0]["description"]
+    wind_speed = data["wind"]["speed"]
+
+    print(f"Weather in {CITY}:")
+    print(f"Temperature: {temp}°C")
+    print(f"Humidity: {humidity}%")
+    print(f"Condition: {weather_desc.capitalize()}")
+    print(f"Wind Speed: {wind_speed} m/s")
+else:
+    print("Error:", data.get("message", "Failed to retrieve data"))
